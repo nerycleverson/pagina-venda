6e31467e2ebbdd211ac8957ecdf36769e70d1846
nerycleverson <nerycleverson@gmail.com>
2026-06-20 14:54:14 +0000

    Você colocou o pixel direto no código ou O código está usando variável?

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -3,7 +3,7 @@ import Script from "next/script";
 export function Analytics() {
   const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID;
-  const tiktokPixelId = "D8GRM6BC77U5P5NBPEG0";
+  const tiktokPixelId = process.env.NEXT_PUBLIC_TIKTOK_PIXEL_ID;
 
   const gaScript =
     "window.dataLayer = window.dataLayer || [];" +
@@ -20,13 +20,13 @@ export function Analytics() {
     "(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');" +
     "fbq('init'," + JSON.stringify(metaPixelId) + ");fbq('track','PageView');";
 
-  const tiktokScript = `
+  const tiktokScript = tiktokPixelId ? `
     !function (w, d, t) {
       w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};var o=document.createElement("script");o.type="text/javascript",o.async=!0,o.src=i+"?sdkid="+e+"&lib="+t;var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(o,a)};
       ttq.load('${tiktokPixelId}');
       ttq.page();
     }(window, document, 'ttq');
-  `;
+  ` : "";
 
   return (
     <>
